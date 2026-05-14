1fcf9ad3014c6e1689a10f03cbcff312e320ab96
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-14 07:19:39 -0500

    dumbass tempo art

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,25 @@
 package;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	var bg:FlxSprite;
+	var robot:FlxSprite;
+	var desk:FlxSprite;
+
 	override public function create()
 	{
 		super.create();
+
+		bg = new FlxSprite(0, 0, 'assets/images/bg.png');
+		robot = new FlxSprite(0, 0, 'assets/images/robot.png');
+		desk = new FlxSprite(0, 0, 'assets/images/desk.png');
+
+		add(bg);
+		add(robot);
+		add(desk);
 	}
 
 	override public function update(elapsed:Float)
