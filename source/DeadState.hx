3e0b8990f538c16b31e14e405205240ab3c1fa5d
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-14 15:54:37 -0500

    just smth dumb

--- source/DeadState.hx
+++ source/DeadState.hx
@@ -1,5 +1,16 @@
 package;
 
+import flixel.text.FlxText;
 import flixel.FlxState;
 
-class DeadState extends FlxState {}
+class DeadState extends FlxState
+{
+	override function create()
+	{
+		super.create();
+
+		var text = new FlxText(0, 0, 0, 'haha u dead lmao', 16);
+		add(text);
+		text.screenCenter();
+	}
+}
