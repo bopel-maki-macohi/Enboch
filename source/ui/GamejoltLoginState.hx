012b96df0627e4f72286d51da4ab9023c4c4db4a
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-16 07:38:19 -0500

    i forgot the project xml thing for flixel-ui

--- source/ui/GamejoltLoginState.hx
+++ source/ui/GamejoltLoginState.hx
@@ -1,9 +1,12 @@
 package ui;
 
+import flixel.addons.ui.FlxUIInputText;
 import utilShitsie.EnboState;
 
 class GamejoltLoginState extends EnboState
 {
+    var usernameInput:FlxUIInputText;
+
 	override function create()
 	{
 		super.create();
