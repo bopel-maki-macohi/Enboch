04e5c4d35cdbea824f6ff51fa7432452ee54ce7c
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-16 15:10:10 -0500

    dsfdscdfsdcsdsdsdaf

--- source/utilShitsie/api/scoreboards/Scoreboards.hx
+++ source/utilShitsie/api/scoreboards/Scoreboards.hx
@@ -4,15 +4,9 @@ import utilShitsie.api.scoreboards.Scoreboard;
 
 class Scoreboards
 {
-	public static var WAIT_TIME_DROWNED:Scoreboard = new Scoreboard(1083904, 'WT_Drowned');
-
 	public static function WAIT_TIME(char:String)
 	{
-		switch (char.toLowerCase())
-		{
-			case 'drowned':
-				return WAIT_TIME_DROWNED;
-		}
+		switch (char.toLowerCase()) {}
 
 		return null;
 	}
