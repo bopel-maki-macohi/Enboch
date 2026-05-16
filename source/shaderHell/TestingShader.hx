f68ec320a60400495d3f09c8eff20f5dbc45f057
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-16 17:46:08 -0500

    I have atleast learned SOMETHING on shaders and its getting the image to show

--- source/shaderHell/TestingShader.hx
+++ source/shaderHell/TestingShader.hx
@@ -7,7 +7,13 @@ class TestingShader extends FlxShader
 	@:glFragmentSource('
 		#pragma header
 
-        void main() {}
+        void main()
+		{
+	        vec2 st = openfl_TextureCoordv.xy;  // Note, already normalized
+	
+			vec4 color = flixel_texture2D(bitmap, st);	
+			gl_FragColor = color;
+		}
 	')
 	override public function new()
 	{
