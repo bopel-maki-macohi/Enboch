40c520775f824d6789baedd15f62a8ffbfaa4a51
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-15 23:08:46 -0500

    mouse hidden

--- source/InitState.hx
+++ source/InitState.hx
@@ -1,3 +1,4 @@
+import flixel.FlxG;
 import utilShitsie.ScreenshotPlugin;
 import flixel.util.typeLimit.NextState.InitialState;
 import flixel.FlxGame;
@@ -7,6 +8,8 @@ class InitState extends FlxGame
 	override public function new(startingState:InitialState)
 	{
 		super(0, 0, startingState);
+
+		FlxG.mouse.visible = false;
 
         ScreenshotPlugin.init();
 	}
