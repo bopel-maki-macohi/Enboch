95f617dc229aa10d02e750c2a91ff8835354c0e9
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-16 11:35:22 -0500

    more meds

--- source/utilShitsie/trophies/Trophies.hx
+++ source/utilShitsie/trophies/Trophies.hx
@@ -3,4 +3,9 @@ package utilShitsie.trophies;
 class Trophies
 {
 	public static var DROWNED_PLAY:Trophy = new Trophy(299928);
+
+	public static var ONE_DAYCYCLE:Trophy = new Trophy(299965);
+	public static var THREE_DAYCYCLES:Trophy = new Trophy(299963);
+	public static var NINE_DAYCYCLES:Trophy = new Trophy(299964);
+	public static var TWENTY_SEVEN_DAYCYCLES:Trophy = new Trophy(299966);
 }
