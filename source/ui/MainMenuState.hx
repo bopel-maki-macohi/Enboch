b7dc2b7d8318f83ad8eb298039144befe98305c4
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-15 23:16:03 -0500

    bum ass main menu begin prod

--- source/ui/MainMenuState.hx
+++ source/ui/MainMenuState.hx
@@ -1,5 +1,27 @@
 package ui;
 
+import flixel.text.FlxText;
+import flixel.group.FlxSpriteGroup;
 import utilShitsie.EnboState;
 
-class MainMenuState extends EnboState {}
+class MainMenuState extends EnboState
+{
+	public var textGrp:FlxSpriteGroup;
+
+	var entries:Array<String> = ['Levels', 'Options',];
+
+	override function create()
+	{
+		super.create();
+
+		add(textGrp = new FlxSpriteGroup());
+
+		for (entry in entries)
+		{
+			var newText = new FlxText(0, 0, 0, entry, 16);
+			newText.ID = textGrp.length;
+
+			textGrp.add(newText);
+		}
+	}
+}
