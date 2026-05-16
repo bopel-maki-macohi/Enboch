5875cba1dbd4795a196559712d30034a9b019d3c
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-16 07:10:31 -0500

    secret data files for my gamejoly private key

--- source/utilShitsie/api/GamejoltAPI.hx
+++ source/utilShitsie/api/GamejoltAPI.hx
@@ -1,9 +1,12 @@
 package utilShitsie.api;
 
+import macroShit.SecretDataFile;
 import flixel.addons.api.FlxGameJolt;
 
 class GamejoltAPI
 {
+	static var privateKey:String = SecretDataFile.build('dev/api/gamejolt-privateKey');
+
 	public static function init()
 	{
 		FlxGameJolt.init(1070390, '', true);
