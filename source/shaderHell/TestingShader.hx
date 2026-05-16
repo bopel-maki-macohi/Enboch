17cb37c0571c248219521c8cc1face10ec5c74cf
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-16 17:49:25 -0500

    accidentally did something cool lmao

--- source/shaderHell/TestingShader.hx
+++ source/shaderHell/TestingShader.hx
@@ -7,16 +7,24 @@ class TestingShader extends FlxShader
 	@:glFragmentSource('
 		#pragma header
 
-        void main()
+		uniform float u_brightnessThreshold;
+
+		void main()
 		{
-	        vec2 st = openfl_TextureCoordv.xy;  // Note, already normalized
-	
-			vec4 color = flixel_texture2D(bitmap, st);	
-			gl_FragColor = color;
+			vec2 st = openfl_TextureCoordv.xy;  // Note, already normalized
+
+			vec4 color = flixel_texture2D(bitmap, st);
+			if (dot(color.rgb, vec3(0.2126, 0.7152, 0.0722)) > u_brightnessThreshold) {
+				gl_FragColor = color;
+			} else {
+				gl_FragColor = vec4(0.0);
+			}
 		}
 	')
 	override public function new()
 	{
 		super();
+
+		this.u_brightnessThreshold.value = [0.5];
 	}
 }
