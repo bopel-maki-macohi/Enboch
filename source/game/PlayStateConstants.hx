8b6b91bca5267a229212f9d776e362fe6c45d16c
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-17 10:29:08 -0500

    I FIGURED IT OUT YAY

--- source/game/PlayStateConstants.hx
+++ source/game/PlayStateConstants.hx
@@ -3,7 +3,7 @@ package game;
 class PlayStateConstants
 {
 	public static final STATES_MAX:Int = 4;
-	public static final STATES_MIN:Int = 4;
+	public static final STATES_MIN:Int = 3;
 
 	public static final TIMER_CHAR_AI_DEFAULT_LENGTH:Float = 5;
 	public static final TIMER_DEATH_DEFAULT_LENGTH:Float = 3;
