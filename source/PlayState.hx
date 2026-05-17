793f43e6e92bf7b2abe7a9d8b7830dbf716f7898
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-17 10:29:28 -0500

    I shall trust them again

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -46,12 +46,7 @@ class PlayState extends EnboState
 
 		if (config_using_cAM_ro)
 		{
-			this.rng_cAM_ro_max = rngList[4];
-
-			if (rng_cAM_ro_max > config_cAM_ro_max_max)
-				rng_cAM_ro_max = config_cAM_ro_max_max;
-			if (rng_cAM_ro_max < config_cAM_ro_max_min)
-				rng_cAM_ro_max = config_cAM_ro_max_min;
+			this.rng_cAM_ro_max = Math.round(Math.max(Math.min(rngList[4], config_cAM_ro_max_max), config_cAM_ro_max_min));
 		}
 	}
 
@@ -118,10 +113,7 @@ class PlayState extends EnboState
 		for (i => thing in stateChangeChances)
 			trace('$character-phase$i : $thing');
 
-		if (config_states < STATES_MIN)
-			config_states = STATES_MIN;
-		if (config_states > STATES_MAX)
-			config_states = STATES_MAX;
+		config_states = Math.round(Math.max(Math.min(config_states, STATES_MAX), STATES_MIN));
 
 		GamePhaseSprite.loadCharacterAssets(character, config_states);
 	}
@@ -196,8 +188,7 @@ class PlayState extends EnboState
 				if (!thing.contains(rng_stateChangeChance))
 					continue;
 
-				charSpr.state = StateManager.parseMovementCode('${config_states}${i}${charSpr.state}', charSpr.state,
-					(rng_stateJumpChance >= jumpChanceNumber));
+				charSpr.state = StateManager.parseMovementCode('${config_states}${i}${charSpr.state}', charSpr.state, (rng_stateJumpChance >= jumpChanceNumber));
 			}
 		}
 
