b8fe706f2e044ecdcad32faeb38da1e6d0c75d79
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-17 14:46:44 -0500

    fix compiling errs

--- source/utilShitsie/video/DesktopVideo.hx
+++ source/utilShitsie/video/DesktopVideo.hx
@@ -1,3 +1,19 @@
-import flixel.FlxBasic;
+package utilShitsie.video;
 
-class DesktopVideo extends FlxBasic {}
+import flixel.FlxSprite;
+import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
+
+class DesktopVideo extends FlxTypedSpriteGroup<FlxSprite>
+{
+	override function set_alpha(Value:Float):Float
+	{
+		return super.set_alpha(Value);
+	}
+
+    public var looping(default, default):Bool;
+
+	public function new(filePath:String)
+	{
+		super();
+	}
+}
