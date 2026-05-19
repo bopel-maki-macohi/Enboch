bb85da6e847bcf47f0ca4bb48a45578a4f777c93
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-19 16:05:35 -0500

    THE. new levels

--- source/enboch/ui/LevelSelectMenuState.hx
+++ source/enboch/ui/LevelSelectMenuState.hx
@@ -17,7 +17,7 @@ class LevelSelectMenuState extends EnboState
 {
 	public var textGrp:FlxTypedSpriteGroup<FlxText>;
 
-	var entries:Array<String> = ['Drowned', 'Skeleton', 'Guardian',];
+	var entries:Array<String> = ['Drowned', 'Skeleton', 'Guardian', 'Husk', 'Phantom'];
 
 	var camFollow:FlxObject;
 
