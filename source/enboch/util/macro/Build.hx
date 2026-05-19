5355f172379eb154fe99ab6691ccf6a239ca65b8
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-18 21:38:07 -0500

    06

--- source/enboch/util/macro/Build.hx
+++ source/enboch/util/macro/Build.hx
@@ -2,6 +2,8 @@ package enboch.util.macro;
 
 import haxe.macro.Context;
 
+using StringTools;
+
 class Build
 {
 	public static macro function getBuildTime()
@@ -9,9 +11,11 @@ class Build
 		var time:String = '';
 		var date:Date = Date.now();
 
-        var aP:String = (date.getHours() >= 12) ? 'pm' : 'am';
+		var aP:String = (date.getHours() >= 12) ? 'pm' : 'am';
 
-		time = '${date.getHours() % 12}:${date.getMinutes()}:${date.getSeconds()} $aP (${date.getMonth() + 1}/${date.getDate() + 1}/${date.getFullYear()})';
+		time = '${date.getHours() % 12}:${date.getMinutes()}:'
+			+ '${date.getSeconds()}'.lpad('0', 2)
+			+ ' $aP (${date.getMonth() + 1}/${date.getDate() + 1}/${date.getFullYear()})';
 		Context.info('Build Time: ${time}', Context.currentPos());
 
 		return macro $v{time};
