30bfa74ac92d0d0bc67139077d0f34434a6dad2f
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-18 21:36:44 -0500

    add build time cause its cool

---
+++ source/enboch/util/macro/Build.hx
@@ -0,0 +1,19 @@
+package enboch.util.macro;
+
+import haxe.macro.Context;
+
+class Build
+{
+	public static macro function getBuildTime()
+	{
+		var time:String = '';
+		var date:Date = Date.now();
+
+        var aP:String = (date.getHours() >= 12) ? 'pm' : 'am';
+
+		time = '${date.getHours() % 12}:${date.getMinutes()}:${date.getSeconds()} $aP (${date.getMonth() + 1}/${date.getDate() + 1}/${date.getFullYear()})';
+		Context.info('Build Time: ${time}', Context.currentPos());
+
+		return macro $v{time};
+	}
+}
