9e98a429a1f6d2a3c7ddfe8492f3fa586eae11f5
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-18 21:45:42 -0500

    remove unused func

--- source/enboch/util/GraphicUtil.hx
+++ source/enboch/util/GraphicUtil.hx
@@ -4,15 +4,6 @@ import flixel.graphics.FlxGraphic;
 
 class GraphicUtil
 {
-	public static function destroyGraphics(graphicsList:Array<FlxGraphic>)
-	{
-		for (graphic in graphicsList)
-		{
-			graphic?.destroy();
-			graphicsList.remove(graphic);
-		}
-	}
-
 	public static function persistGraphics(graphicsList:Array<FlxGraphic>)
 	{
 		for (graphic in graphicsList)
