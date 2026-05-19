9fee4edbe70edbf407f82c7fbf56247ccb2ed2c5
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-19 06:07:10 -0500

    not as decked out but it's still cool

--- source/enboch/util/video/DesktopVideo.hx
+++ source/enboch/util/video/DesktopVideo.hx
@@ -8,13 +8,12 @@ import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
 import hxvlc.flixel.FlxVideoSprite;
 #end
 
-class DesktopVideo extends FlxTypedSpriteGroup<FlxSprite>
+class DesktopVideo extends FlxTypedSpriteGroup<FlxSprite> #if hxvlc implements IVideo<FlxVideoSprite> #end
 {
-	public var settings:VideoSettings;
-
 	#if hxvlc
 	public var video:FlxVideoSprite;
 	#end
+	public var settings:VideoSettings;
 
 	public function new(settings:VideoSettings)
 	{
@@ -94,20 +93,36 @@ class DesktopVideo extends FlxTypedSpriteGroup<FlxSprite>
 		#end
 	}
 
+	#if hxvlc
 	public function startVideo()
 	{
-		video.play();
+		if (video != null)
+			video.play();
+	}
+
+	public function pauseVideo()
+	{
+		if (video != null)
+			video.pause();
+	}
+
+	public function resumeVideo()
+	{
+		if (video != null)
+			video.resume();
 	}
 
 	public function restartVideo()
 	{
-		video.bitmap.time = 0;
-		video.resume();
+		if (video != null)
+		{
+			video.bitmap.time = 0;
+			video.resume();
+		}
 	}
 
 	public function finishVideo()
 	{
-		#if hxvlc
 		if (video == null)
 			return;
 
@@ -120,6 +135,6 @@ class DesktopVideo extends FlxTypedSpriteGroup<FlxSprite>
 		video.stop();
 		remove(video);
 		video.destroy();
-		#end
 	}
+	#end
 }
